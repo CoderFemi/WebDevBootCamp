be7ea06c71acae42cd95c4c50ab4e49c52d027fb
Femi Agbebi <olufemiagbebi@gmail.com>
2021-02-23 11:48:16 +0100

    Add cluster map to campgrounds page

--- projects/yelp_camp/controllers/campgrounds.js
+++ projects/yelp_camp/controllers/campgrounds.js
@@ -6,7 +6,7 @@ const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 
 
 const index = async (req, res) => {
-    const campgrounds = await Campground.find({});
+    const campgrounds = await Campground.find({}).populate('popupText');
     res.render('campgrounds/index', { campgrounds })
 }
 
